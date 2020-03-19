a03961f282e3509abe41add8be04fa435f72341c
Sazzad Shopno <Sazzadshopno6@gmail.com>
2020-03-19 12:52:50 +0600

    user as custom model

--- teacher/models.py
+++ teacher/models.py
@@ -1,16 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class Teacher(models.Model):
+class TeacherManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not username:
+            raise ValueError("User must have a username")
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            password = password,
+            username = username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
+        return user
+class Teacher(AbstractBaseUser):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
-    user_name = models.CharField(max_length = 20, primary_key= True)
-    password = models.CharField(max_length = 20)
+    email = models.EmailField(max_length = 50, unique = True)
+    username = models.CharField(max_length = 20, primary_key= True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = TeacherManager()
 
     class Meta:
         ordering = ['first_name']
 
+
+    def has_perm(self, perm, obj= None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
+
     def __str__(self):
-        return "%s %s" % (self.first_name, self.last_name)
+        return  self.username
 
 class Batch(models.Model):
     semester_id = models.CharField(max_length = 20, primary_key = True, null = False)
